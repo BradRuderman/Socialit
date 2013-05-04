aa84473dd3737422f46d7bbe3a236ebd223a60b7
Brad H Ruderman <bradruderman@gmail.com>
2013-05-04 11:06:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Rails App with user login and sessions
+
+This is a template that can be used to build your next Ruby on Rails app! It includes user signup and login features.
+
+Developed by Alex De Simone on 10/11/2012.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '10a01d263038ff07c81a64a78cd110dc53aaa69f7df649ea5da3ab4250e28c57886c32c7593a4a69aeb5841c7eff76843b50b2ba67bd26cb50f8d5f9443f3a1f'
