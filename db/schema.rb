b232a89030109b70d3a42cc3f43984a4b3888137
Brett Goldstein <brettgoldstein3@gmail.com>
2013-05-04 18:01:14 -0700

    updated CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121012065412) do
+ActiveRecord::Schema.define(:version => 20130504210312) do
+
+  create_table "books", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.integer  "price"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
